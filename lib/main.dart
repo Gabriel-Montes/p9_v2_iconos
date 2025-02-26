be4305830f16ae76f6cd789fec7db06eb70b00d2
GABRIEL_ALEJANDRO_MONTES_HERNANDEZ <a.22308051281276@cbtis128.edu.mx>
2025-02-26 20:30:07 +0000

    iconos

--- lib/main.dart
+++ lib/main.dart
@@ -8,23 +8,90 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Gabriel Montes ',
+          ), //Mat:22308051281276 Gpo 6J
+          titleTextStyle: const TextStyle(fontSize: 30, color: Colors.black),
+          centerTitle: true,
+          backgroundColor: Colors.black26,
+        ),
+        body: const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text(
+                'Gabriel Montes Mat:22308051281276',
+                style: (TextStyle(fontSize: 19, color: Colors.black)),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              // Fila 1
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      Icon(Icons.home, size: 40),
+                      Text('Casa'),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(Icons.search, size: 40),
+                      Text('Buscar'),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(Icons.music_note, size: 40),
+                      Text('Musica'),
+                    ],
+                  ),
+                ],
+              ),
+              // Fila 2
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      Icon(Icons.settings, size: 40),
+                      Text('Ajustes'),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(Icons.favorite, size: 40),
+                      Text('Favoritos'),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(Icons.account_circle, size: 40),
+                      Text('Perfil'),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
